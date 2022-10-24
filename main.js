9e01004c393dc7679fd84257ff714e4e955cd164
jellywiz <muzaffer.dler@gmail.com>
2022-10-24 23:00:01 +0300

    Add function add and remove

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-let book = [{
+const title = document.getElementById('title');
+const aurthor = document.getElementById('aurthor');
+const addbtn = document.getElementById('add');
+
+const book = [{
   title: 'The Hobbit',
   aurthor: 'J.R.R. Tolkien',
 },
@@ -8,3 +12,18 @@ let book = [{
   aurthor: 'Douglas Crockford',
 },
 ];
+
+function Book(title, aurthor) {
+  this.title = title;
+  this.aurthor = aurthor;
+}
+
+function addBook(title, aurthor) {
+  const newBook = new Book(title, aurthor);
+  book.push(newBook);
+}
+
+function removeBook(title, aurthor) {
+  const newBook = new Book(title, aurthor);
+  return book.title !== newBook.title && book.aurthor !== newBook.aurthor;
+}
